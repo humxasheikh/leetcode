1bf7ac60c142a90d7eb0bf6aca02e9ebeb02a5ea
Hamza Arshad <hamza.arshad1008@gmail.com>
2025-07-20 23:08:46 +0200

    added the implementation to find the single number from the array

--- array/easy/singleNumber.ts
+++ array/easy/singleNumber.ts
@@ -13,6 +13,17 @@ function singleNumber(nums: number[]): number {
   return 0;
 }
 
+//copied for the internet xd
+const optimalSolution = (nums: number[]): number => {
+  let result = 0;
+  for (const n of nums) result = result ^ n;
+  return result;
+};
+
 console.log(singleNumber([2, 2, 1]));
 console.log(singleNumber([4, 1, 2, 1, 2]));
 console.log(singleNumber([1]));
+
+console.log(optimalSolution([2, 2, 1]));
+console.log(optimalSolution([4, 1, 2, 1, 2]));
+console.log(optimalSolution([1]));
